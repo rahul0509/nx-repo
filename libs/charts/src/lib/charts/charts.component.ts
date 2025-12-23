74251cac2bab7c595a1abc4f9116646332279f9c
Rahul Prajapati <35000407+rahul0509@users.noreply.github.com>
2025-12-23 19:48:44 +0530

    D3 charts demo (#7)
    * d3-charts-demo: Remove redundent files
    
    * Setup charts libs

---
+++ libs/charts/src/lib/charts/charts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-charts',
+  imports: [CommonModule],
+  templateUrl: './charts.component.html',
+  styleUrl: './charts.component.scss',
+})
+export class ChartsComponent {}
